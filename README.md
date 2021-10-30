92d5329d2dc9f5a30e5736267e605f60154371b2
KyleKun <caiofapedroso@gmail.com>
2021-10-30 11:03:01 -0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# happy_luna
+
+Happy Luna is a short & sweet game made for the 1st Flame Engine Game Jam.
---
+++ android/app/src/main/kotlin/com/example/happy_luna/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.happy_luna
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
